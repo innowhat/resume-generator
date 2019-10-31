7d1ce9b1380523a1643af73b2c19e7a9e4684daf
innowhat <innowhat@gmail.com>
2019-10-31 22:45:40 +0200

    Updates

---
+++ precache-manifest.5f39645bf95dfdfad8ad3ed1a5d1d42a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd13ae57153b1fb03c199dc2776bd256",
+    "url": "/resume-generator/index.html"
+  },
+  {
+    "revision": "b61117505280f06d1ac9",
+    "url": "/resume-generator/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "b61117505280f06d1ac9",
+    "url": "/resume-generator/static/js/2.37b182b8.chunk.js"
+  },
+  {
+    "revision": "3d49a5b9c4c59d468d02",
+    "url": "/resume-generator/static/js/main.381d4eb0.chunk.js"
+  },
+  {
+    "revision": "601d8d11ee54251384d7",
+    "url": "/resume-generator/static/js/runtime~main.cecf1bce.js"
+  }
+]);
