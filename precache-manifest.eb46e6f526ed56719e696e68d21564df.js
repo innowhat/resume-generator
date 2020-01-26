ba100790c976dbf1313a9c7bdc8081859c38c919
innowhat <innowhat@gmail.com>
2020-01-26 13:36:00 +0200

    Updates

---
+++ precache-manifest.eb46e6f526ed56719e696e68d21564df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444ee6a02cede44aaa1344ef77de86a8",
+    "url": "/resume-generator/index.html"
+  },
+  {
+    "revision": "b57c45ce1cd32b19287c",
+    "url": "/resume-generator/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "b57c45ce1cd32b19287c",
+    "url": "/resume-generator/static/js/2.3303229e.chunk.js"
+  },
+  {
+    "revision": "16a7120e26236c66df4cb4662917f6af",
+    "url": "/resume-generator/static/js/2.3303229e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9758136134102771470e",
+    "url": "/resume-generator/static/js/main.ddf74d33.chunk.js"
+  },
+  {
+    "revision": "e617b88842ae87741a2a",
+    "url": "/resume-generator/static/js/runtime-main.c3b8ae87.js"
+  }
+]);
